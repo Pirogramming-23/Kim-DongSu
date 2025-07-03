617645ca8b68f0a2e07798d443100ab782cbdc62
Github GGo-Dong <kimdongsu481@gmail.com>
2025-07-04 02:02:00 +0900

    1번 5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -2,23 +2,43 @@
 num = 0
 
 #2단계
-user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+userA_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
 #3단계
 while True:
     try:
-        user_call = int(user_input)
+        userA_call = int(userA_input)
     except:
         print("정수를 입력하세요")
-        user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        userA_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
         continue
-    if user_call != 1 and user_call != 2 and user_call != 3:
+    if userA_call != 1 and userA_call != 2 and userA_call != 3:
         print("1,2,3 중 하나를 입력하세요")
-        user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        userA_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
         continue
     break
 
 #4단계
-for i in range(user_call):
+for i in range(userA_call):
     num += 1
     print(f'playerA : {num}')
+
+#5단계
+userB_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+while True:
+    try:
+        userB_call = int(userB_input)
+    except:
+        print("정수를 입력하세요")
+        userB_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        continue
+    if userB_call != 1 and userB_call != 2 and userB_call != 3:
+        print("1,2,3 중 하나를 입력하세요")
+        userB_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        continue
+    break
+
+for i in range(userB_call):
+    num += 1
+    print(f'playerB : {num}')
